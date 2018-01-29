b8f459eade517ac08d86d47637e27a84138cdc8a
Joseph Weidinger <jsphweid@gmail.com>
2018-01-29 12:25:11 -0800

    not dry run

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 bucket_name=$(<~/.aws/main_bucket_name)
 cloudfront_id=$(<~/.aws/main_cloudfront_id)
 
-aws s3 sync ./build/ s3://${bucket_name}/browserTranscribe --delete --dryrun
+aws s3 sync ./build/ s3://${bucket_name}/browserTranscribe --delete
 aws cloudfront create-invalidation --distribution-id ${cloudfront_id} --paths "/*"
